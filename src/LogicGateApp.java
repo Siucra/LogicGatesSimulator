9a58d9dc3b1694759e0db25752a408f2e340c047
DB <131527960+Siucra@users.noreply.github.com>
2025-04-03 10:51:21 +0100

    Implemented LogicGateGUI() and LogicGateApp()
    Implemented LogicGateGUI method that handles the Graphical User Interface with some functionality around retrieving input and displaying the output.
    
    Implemented a LogicGateApp which acts as the main application for the programme with a main method that calls the LogicGateGUI to display.

--- src/LogicGateApp.java
+++ src/LogicGateApp.java
@@ -1,3 +1,4 @@
+import javax.swing.SwingUtilities;
 
 public class LogicGateApp {
 	//Main application that handles logic gates
@@ -17,6 +18,15 @@ public class LogicGateApp {
 	
 	public static boolean xorGate(boolean a, boolean b) {
 		return a ^ b; //Exclusive OR
+		
+	}
+	
+	public static void main(String[]args) {
+		//()-> shortcut for  writing small function with no input
+		//() -> called a lambda : takes no parameters,
+		//-> seperates the parameter from the code i want to run
+		
+		SwingUtilities.invokeLater(() -> new LogicGateGUI().setVisible(true));
 		
 	}
 }
--- src/LogicGateGUI.java
+++ src/LogicGateGUI.java
@@ -1,4 +1,71 @@
+import javax.swing.*; //Provides GUI Components
+import java.awt.*; //Provides basic GUI tools
+import java.awt.event.*; // Provides event listeners
 
-public class LogicGateGUI {
+//JFrame is the basic SWING window 
+public class LogicGateGUI extends JFrame{
+	
+	private JCheckBox inputA, inputB; //checkboxes
+	private JLabel outputLabel; // displays output
+	private JComboBox<String> gateSelector; //dropdown menu to choose logic gate
+	
+	public LogicGateGUI() {
+		//constructor for GUI layout
+		setTitle("My Logic Gate Simulator");//title
+		setSize(400,200); //size of screen
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE); //exits program when window is closed
+		setLayout(new FlowLayout());
+		
+		//declaring variables
+		inputA = new JCheckBox("Input A");
+		inputB = new JCheckBox("Input B");
+		outputLabel = new JLabel("Output: false");//checkbox initial false value
+		
+		String[] gates = {"AND", "OR", "XOR", "NOT (A only)"};
+		gateSelector = new JComboBox<>(gates);
+		
+		JButton evaluateButton = new JButton("Evaluate");//creates a combo box with gate names
+		evaluateButton.addActionListener(e -> evaluateGate());//listener event
+		
+		//adds each gui component
+		add(gateSelector);
+		add(inputA);
+		add(inputB);
+		add(outputLabel);
+		add(evaluateButton);
+		
+	}
 
+	public void evaluateGate() {
+		//gets the value of the checkboxes, if checked set to true
+		boolean a = inputA.isSelected();
+		boolean b = inputB.isSelected();
+		
+		//gets selected item from dropdown menu
+		String selectedGate = (String) gateSelector.getSelectedItem();
+		boolean result = false;
+		
+		switch(selectedGate) {
+			case "AND":{
+				result = LogicGateApp.andGate(a,b);
+				break;//exits loop
+			}
+			case "OR":{
+				result = LogicGateApp.orGate(a,b);
+				break;//exits loop
+			}
+			case "XOR":{
+				result = LogicGateApp.xorGate(a, b);
+				break;//exits loop
+			}
+			case "NOT (A only)":{
+				result = LogicGateApp.notGate(a);
+				break;//exits loop
+			}
+		}
+		outputLabel.setText("Output: " + result);//display result
+	}
+	
+	
 }
