722b11a76afc5a70fb3fbb8a604a77cd14ebe7db
Meep. <131527960+Siucra@users.noreply.github.com>
2025-05-27 14:09:10 +0100

    svgSalamander JAR
    svgSalamander JAR to support SVG-based logic gate rendering

---
+++ src/SVGViewer.java
@@ -0,0 +1,11 @@
+import com.kitfox.svg.SVGDiagram;
+import com.kitfox.svg.SVGUniverse;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.net.URI;
+
+public class SVGViewer {
+
+}
