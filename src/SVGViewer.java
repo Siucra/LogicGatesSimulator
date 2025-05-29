7ec30bbec8287f3bd3b257fd2058263c0ad0593e
Meep. <131527960+Siucra@users.noreply.github.com>
2025-05-29 14:29:34 +0100

    SVGViewer class Updated
    Updated the SVGViewer class to center and scale all SVGs within the SVGViewer panel.

--- src/SVGViewer.java
+++ src/SVGViewer.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.net.URI;
-import java.net.URL;
 import com.kitfox.svg.app.beans.SVGIcon;
 
 public class SVGViewer extends JPanel {
@@ -14,8 +13,7 @@ public class SVGViewer extends JPanel {
 
     public void loadSVG(File file) {
         try {
-            URI uri = file.toURI();
-            icon.setSvgURI(uri);
+            icon.setSvgURI(file.toURI());
             repaint();
         } catch (Exception e) {
             e.printStackTrace();
@@ -24,9 +22,38 @@ public class SVGViewer extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (icon != null) {
-            icon.paintIcon(this, g, 0, 0);
+        super.paintComponent(g); //superclass method clears the panel and prepares for a new drawing
+
+        if (icon != null && icon.getIconWidth() > 0 && icon.getIconHeight() > 0) {//checks if svg has valid dimensions
+            Graphics2D g2 = (Graphics2D) g.create();//copies the original to scale without affecting original svg
+
+            //gets current size of panel screen
+            int panelWidth = getWidth();
+            int panelHeight = getHeight();
+
+            //original width and height of svg
+            int iconWidth = icon.getIconWidth();
+            int iconHeight = icon.getIconHeight();
+
+            //calcs how much svg has to scale to fit panel
+            double scaleX = (double) panelWidth / iconWidth;
+            double scaleY = (double) panelHeight / iconHeight;
+            
+            double scale = Math.min(scaleX, scaleY); // maintain aspect ratio
+
+            //calcs pixel dimensions of scaled svg
+            int scaledWidth = (int) (iconWidth * scale);
+            int scaledHeight = (int) (iconHeight * scale);
+
+            //centres on panel
+            int x = (panelWidth - scaledWidth) / 2;
+            int y = (panelHeight - scaledHeight) / 2;
+
+            g2.translate(x, y);
+            g2.scale(scale, scale);
+            icon.paintIcon(this, g2, 0, 0);
+
+            g2.dispose();
         }
     }
 }
